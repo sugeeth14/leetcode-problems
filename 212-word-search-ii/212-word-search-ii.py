a3a47d31dd8130bb265b8c7c18acfd9c9fc7ddf4
Sugeeth <raghava.sugeeth@gmail.com>
2022-12-03 01:46:14 -0700

    Time: 4746 ms (27.25%), Space: 18.7 MB (28.73%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.py
+++ 212-word-search-ii/212-word-search-ii.py
@@ -1,42 +1,69 @@
-class Solution:
-    def findWords(self, board, words):
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.islast = False
+    
+    def insert(self, word):
+        cur = self
+        for char in word:
+            if char not in cur.children:
+                cur.children[char] = TrieNode()
+            cur = cur.children[char]
+        cur.islast = True
+    
+    def delete(self, word):
+        # Here we must recursively delete the word
         
-        
-        words_set = set()
-        
-        longest_length = 0
-        for word in words:
-            longest_length = max(longest_length, len(word))
-        rows = len(board)
-        columns = len(board[0])
-        
-        def backtrack(current_word, x, y, path):
-            if x < 0 or x == rows or y <0 or y == columns:
-                return
-            if (x, y) in path:
+        def backtrack(cur, index):
+            if index == len(word):
+                # If this is the last character delete the node
+                cur.islast = False
                 return
             else:
-                path.add((x,y))
-                current_word += board[x][y]
-                words_set.add(current_word)
-                if len(current_word) == longest_length:
-                    path.remove((x,y))
-                    return
-                else:
-                    backtrack(current_word, x + 1, y, path)
-                    backtrack(current_word, x - 1, y, path)
-                    backtrack(current_word, x, y + 1, path)
-                    backtrack(current_word, x, y-1, path)
-                    path.remove((x,y))
+                backtrack(cur.children[word[index]], index + 1)
+                if len(cur.children[word[index]].children) == 0:
+                    del cur.children[word[index]]
+        backtrack(self, 0)
                 
-                # current_word = current_word[:-1]
-                # path = path[:-1]
-        for i in range(rows):
-            for j in range(columns):
-                backtrack("", i, j, set())
-        res = []
+            
+                
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        
+        self.root = TrieNode()
         for word in words:
-            if word in words_set:
-                res.append(word)
-        return res
+            self.root.insert(word)
+        
+        
+        rows = len(board)
+        cols = len(board[0])
+        
+        res = set()
+        visit = set()
+        
+        def dfs(i, j, node, word):
+            if i < 0 or i == rows or j < 0 or j == cols:
+                return
+            if (i, j) in visit:
+                return
+            if board[i][j] not in node.children:
+                return
+            
+            visit.add((i, j))
+            word += board[i][j]
+            node = node.children[word[-1]]
+            if node.islast:
+                res.add(word)
+                self.root.delete(word)
+            dfs(i-1, j, node, word)
+            dfs(i + 1, j, node, word)
+            dfs(i, j + 1, node, word)
+            dfs(i, j - 1, node, word)
+            
+            
+            visit.remove((i, j))
+        for i in range(rows):
+            for j in range(cols):
+                dfs(i, j, self.root, "")
+        return list(res)
         
