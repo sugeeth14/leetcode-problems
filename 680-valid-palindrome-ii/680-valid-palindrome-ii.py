f5c105e0b64186f78dbb9e8d7ab97e8e5601f23b
Sugeeth <raghava.sugeeth@gmail.com>
2023-04-13 17:46:49 -0600

    Time: 187 ms (12.61%), Space: 14.4 MB (81.87%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        
+        def checkPalindrome(current_string):
+            l = 0
+            r = len(current_string) - 1
+            while l < r:
+                if current_string[l] != current_string[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            if s[l] == s[r]:
+                l += 1
+                r -= 1
+            else:
+                # We have two string choices here either not considering the character at l or not considering the character at r
+                return checkPalindrome(s[:l] + s[l + 1:]) or checkPalindrome(s[:r] + s[r+1:])
+        return True
+        
