3f9dabdd090b6807fcd1169e565e2d3586398849
Sugeeth <raghava.sugeeth@gmail.com>
2023-04-13 12:30:24 -0600

    Time: 39 ms (38.00%), Space: 13.8 MB (87.74%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -5,39 +5,12 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        levels = {}
-        
-        from collections import deque
-        queue = deque()
-        
-        queue.append((root, 0))
-        while queue:
-            top, level = queue.popleft()
-            if level in levels:
-                levels[level].append(top)
-            else:
-                levels[level] = [top]
-            if top:
-                queue.append((top.left, level+ 1))
-                queue.append((top.right, level + 1))
-        for key in levels:
-            if key != 0:
-                # Check if the level forms a palindrome
-                l = 0
-                r = len(levels[key]) - 1
-                while l < r:
-                    if not levels[key][l] and not levels[key][r]:
-                        pass
-                    elif not levels[key][l]:
-                        return False
-                    elif not levels[key][r]:
-                        return False
-                    elif levels[key][l].val != levels[key][r].val:
-                        return False
-                    l += 1
-                    r -= 1
-        return True
-        
-        
-        
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def is_symmetric(node1, node2):
+            if node1 == None and node2 == None:
+                return True
+            if node1 == None or node2 == None:
+                return False
+            if node1.val == node2.val:
+                return is_symmetric(node1.left, node2.right) and is_symmetric(node1.right, node2.left)
+        return is_symmetric(root.left, root.right)
