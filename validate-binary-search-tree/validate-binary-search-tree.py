3cd3da11b311dd5be34fa48a5c372e2f4c090e27
Sugeeth <raghava.sugeeth@gmail.com>
2023-04-12 19:56:30 -0600

    Time: 47 ms, Memory: 17.1 MB - LeetHub

--- validate-binary-search-tree/validate-binary-search-tree.py
+++ validate-binary-search-tree/validate-binary-search-tree.py
@@ -8,30 +8,18 @@ class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         
         
-        def isBinary(node):
-            if not node.left and not node.right:
-                return (True, node.val, node.val)
-            elif node.left and node.right:
-                leftBinary, leftMax, leftMin = isBinary(node.left)
-                rightBinary, rightMax, rightMin = isBinary(node.right)
-                if leftBinary and rightBinary and leftMax < node.val < rightMin:
-                    return (True, rightMax, leftMin)
-                else:
-                    return (False, -1, -1)
-            elif node.left:
-                leftBinary, leftMax, leftMin = isBinary(node.left)
-                if leftBinary and leftMax < node.val:
-                    return (True, node.val, leftMin)
-                else:
-                    return (False, -1, -1)
-            elif node.right:
-                rightBinary, rightMax, rightMin = isBinary(node.right)
-                if rightBinary and rightMin > node.val:
-                    return (True, rightMax, node.val)
-                else:
-                    return (False, -1, -1)
-                
-        return isBinary(root)[0]
-                    
-                
+        path = []
+        def inorder(node):
+            if not node:
+                return
+            
+            inorder(node.left)
+            path.append(node.val)
+            inorder(node.right)
+        inorder(root)
         
+        for i in range(1, len(path)):
+            if path[i] <= path[i-1]:
+                return False
+        return True
+                
