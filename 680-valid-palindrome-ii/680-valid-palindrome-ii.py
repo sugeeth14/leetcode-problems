1704f40116f583be75f6f432e011af70f6f0408c
Sugeeth <raghava.sugeeth@gmail.com>
2023-04-14 14:38:14 -0600

    Time: 156 ms (36.49%), Space: 14.6 MB (36.72%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -2,7 +2,6 @@ class Solution:
     def validPalindrome(self, s: str) -> bool:
         
         def checkPalindrome(l, r):
-
             while l < r:
                 if s[l] != s[r]:
                     return False
@@ -10,13 +9,11 @@ class Solution:
                 r -= 1
             return True
         
-        
         l = 0
         r = len(s) - 1
         while l < r:
             if s[l] != s[r]:
-                return checkPalindrome(l+1, r) or checkPalindrome(l, r-1)
+                return checkPalindrome(l + 1, r) or checkPalindrome(l, r-1)
             l += 1
             r -= 1
         return True
-        
