b2e8f87c0f016a51e075a6459bd92be8a7f72c2d
Sugeeth <raghava.sugeeth@gmail.com>
2023-04-16 17:28:44 -0600

    Time: 82 ms (67.90%), Space: 17.2 MB (85.33%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,42 +1,14 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        # first append a dummy to the result
-        if len(intervals) == 0:
-            return [newInterval]
-        inserted = False
         res = []
         for i in range(len(intervals)):
-            if newInterval[0] <= intervals[i][0]:
-                # Merge the new interval with previous before appending
-                if not res:
-                    res.append(newInterval)
-                else:
-                    if newInterval[0] <= res[-1][1]:
-                        res[-1][1] = max(res[-1][1], newInterval[1])
-                    else:
-                        res.append(newInterval)
-                inserted = True
-                break
-            else:
-                res.append(intervals[i])
-        
-        # it will break at a certain i
-        if not inserted:
-            # It has not been merged
-            if newInterval[0] <= res[-1][1]:
-                res[-1][1] = max(res[-1][1], newInterval[1])
-            else:
+            if newInterval[1] < intervals[i][0]:
                 res.append(newInterval)
-            return res
-        # print(res)
-        while i < len(intervals):
-            prevStart, prevEnd = res[-1][0], res[-1][1]
-            if intervals[i][0] <= prevEnd:
-                # There is overlap
-                res[-1] = [prevStart, max(prevEnd, intervals[i][1])]
-            else:
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
                 res.append(intervals[i])
-            i += 1
+            else:
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+        res.append(newInterval)
         return res
-            
         
