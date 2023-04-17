4d694d2cef2482017d64deaf566d12caf720111c
Sugeeth <raghava.sugeeth@gmail.com>
2023-04-16 18:13:54 -0600

    Time: 38 ms (95.01%), Space: 17 MB (24.43%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -6,32 +6,18 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        res = []
+        def inorder(node):
+            if not node:
+                return
+            inorder(node.left)
+            res.append(node.val)
+            inorder(node.right)
+        inorder(root)
         
-        
-        def isBinary(node):
-            if not node.left and not node.right:
-                return (True, node.val, node.val)
-            elif node.left and node.right:
-                leftBinary, leftMax, leftMin = isBinary(node.left)
-                rightBinary, rightMax, rightMin = isBinary(node.right)
-                if leftBinary and rightBinary and leftMax < node.val < rightMin:
-                    return (True, rightMax, leftMin)
-                else:
-                    return (False, -1, -1)
-            elif node.left:
-                leftBinary, leftMax, leftMin = isBinary(node.left)
-                if leftBinary and leftMax < node.val:
-                    return (True, node.val, leftMin)
-                else:
-                    return (False, -1, -1)
-            elif node.right:
-                rightBinary, rightMax, rightMin = isBinary(node.right)
-                if rightBinary and rightMin > node.val:
-                    return (True, rightMax, node.val)
-                else:
-                    return (False, -1, -1)
-                
-        return isBinary(root)[0]
-                    
-                
+        for i in range(1, len(res)):
+            if res[i] <= res[i-1]:
+                return False
+        return True
+            
         
