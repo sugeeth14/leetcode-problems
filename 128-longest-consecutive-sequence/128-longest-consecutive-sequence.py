f6d2cc64ec38c6bf95ad50399ea15daf739fc925
Sugeeth <raghava.sugeeth@gmail.com>
2023-04-19 16:41:47 -0600

    Time: 2011 ms (28.03%), Space: 28.9 MB (54.72%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -1,41 +1,14 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        
-        seen = set()
-        edges = {}
-        max_length = 0
-        
-        for i in range(len(nums)):
-            if nums[i] in seen:
-                continue
-            else:
-                seen.add(nums[i])
-                # Four cases
-                # Case 1: Both are in seen
-                if nums[i] - 1 in seen and nums[i] + 1 in seen:
-                    edges[nums[i] - 1][1] = edges[nums[i] + 1][1]
-                    edges[nums[i] + 1][0] = edges[nums[i] - 1][0]
-                    edges[edges[nums[i] - 1][0]][1] = edges[nums[i] + 1][1]
-                    edges[edges[nums[i] + 1][1]][0] = edges[nums[i] - 1][0]
-                    edges[nums[i]] = edges[nums[i] - 1]
-                elif nums[i] - 1 in seen:
-                    edges[nums[i] - 1][1] = nums[i]
-                    edges[edges[nums[i] - 1][0]][1] = nums[i]
-                    edges[nums[i]] = edges[nums[i] - 1]
-                elif nums[i] + 1 in seen:
-                    edges[nums[i] + 1][0] = nums[i]
-                    edges[edges[nums[i] + 1][1]][0] = nums[i]
-                    edges[nums[i]] = edges[nums[i] + 1]
-                else:
-                    edges[nums[i]] = [nums[i], nums[i]]
-                
-                # print(nums[i])
-                # print(edges)
+        nums_set = set(nums)
+        longest = 0
+        for num in nums:
+            if (num - 1) not in nums_set:
+                # Then it is the first element
+                length = 0
+                while (num + length) in nums_set:
+                    length += 1
+                longest = max(longest, length)
+        return longest
                     
-                max_length = max(max_length, edges[nums[i]][1] - edges[nums[i]][0])
-                # print(max_length)
-        return max_length + 1
-                
         
