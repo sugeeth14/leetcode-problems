d38a9bfb42511f14245d168e95bc3b3923eb3b3e
Sugeeth <raghava.sugeeth@gmail.com>
2023-05-26 20:53:17 -0600

    Time: 56 ms (51.80%), Space: 16.3 MB (51.80%) - LeetHub

--- 877-stone-game/877-stone-game.py
+++ 877-stone-game/877-stone-game.py
@@ -1,44 +1,6 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        alice_score = 0
-        bob_score = 0
-        
-        current_player = 0
-        l = 0
-        r = len(piles) - 1
-        
-        while l <= r:
-            if current_player % 2 == 0:
-                if piles[l + 1] > piles[r - 1]:
-                    alice_score += piles[l]
-                    l += 1
-                elif piles[l + 1] < piles[r - 1]:
-                    alice_score += piles[r]
-                    r -= 1
-                else:
-                    if piles[l] >= piles[r]:
-                        alice_score += piles[l]
-                        l += 1
-                    else:
-                        alice_score += piles[r]
-                        r -= 1
-                current_player += 1
-            else:
-                if piles[l + 1] > piles[r - 1]:
-                    bob_score += piles[l]
-                    l += 1
-                elif piles[l + 1] < piles[r - 1]:
-                    bob_score += piles[r]
-                    r -= 1
-                else:
-                    if piles[l] >= piles[r]:
-                        bob_score += piles[l]
-                        l += 1
-                    else:
-                        bob_score += piles[r]
-                        r -= 1
-                current_player += 1
-            return alice_score > bob_score
+        return True
                 
         
         
