7a0d0a956dd0863dffea4442995f2b5a33739ce2
Sugeeth <raghava.sugeeth@gmail.com>
2023-08-07 19:57:17 -0600

    Time: 688 ms (90.39%), Space: 20.5 MB (17.92%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,18 +1,10 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        # Algorithm: We will be using Topological sort
-        # Initialize visited set and dic to keep track of parents of each node
-        # iterate over each nodes and call top sort on that node if that node doesn't have any incoming edges and it is not visited.
-        # during every step before calling the children, populate the dic for the current node and add the current node to the set and call for its children.
-        # Once all the children have been visited just return. 
-        # At the end you will have  a dic of set that can be used for a simple look up for queries.
         
         children = collections.defaultdict(list)
-        parents = collections.defaultdict(list)
         
         for req in prerequisites:
             children[req[0]].append(req[1])
-            parents[req[1]].append(req[0])
         
         
         # print(chil)
