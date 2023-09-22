03959f0f74adfbe9ccf27c6437f0803624b92a8f
Sugeeth <raghava.sugeeth@gmail.com>
2023-09-21 21:15:37 -0600

    Time: 974 ms (49.63%), Space: 30.1 MB (97.95%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -1,27 +1,18 @@
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
-        
-        ans = inf
+        max_window = -1
+        target = sum(nums) - x
+        l = 0
         
         current = 0
-        for i in range(len(nums)):
-            current += nums[i]
-            if current >= x:
-                break
-        # print(i)
-        j = len(nums) - 1
-        while i >= 0:
-            # print(i, j)
-            if current == x:
-                ans = min(ans, len(nums)-1-j + i + 1)
-            current -= nums[i]
-            i -= 1
-            while current < x and j > i:
-                current += nums[j]
-                j -= 1
-            # i -= 1
-        # print(current)
-        if current == x:
-            ans = min(ans, len(nums)-1-j + i + 1)
-        return ans if ans != inf else -1
+        for r in range(len(nums)):
+            current += nums[r]
+            
+            while l <= r and current > target:
+                current -= nums[l]
+                l += 1
+            
+            if current == target:
+                max_window = max(max_window, r - l + 1)
+        return -1 if max_window == -1 else len(nums) - max_window
         
