4616b509e07394ed1991a6563552f996a140ebcc
Sugeeth <raghava.sugeeth@gmail.com>
2023-10-24 21:25:21 -0600

    Time: 42 ms (27.32%), Space: 16.2 MB (79.64%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -0,0 +1,18 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        
+        # so there are two choices either move to right or to the left
+        
+        total_elements = 1  << (n-1)
+        current = 0
+        while total_elements != 1:
+            total_elements >>= 1
+            if k <= total_elements:
+                current = current
+            else:
+                current = (current + 1) % 2
+                k -= total_elements
+            # print(total_elements, k)
+        return current
+            
+        
