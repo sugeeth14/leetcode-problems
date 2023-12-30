b8f6e7d07023a0fe81a84d4ed0aa6c07bdb0c7b4
Sugeeth <raghava.sugeeth@gmail.com>
2023-12-29 20:14:30 -0700

    Time: 4977 ms (14.48%), Space: 19.2 MB (7.26%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,29 +1,26 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-
-
-        first_pointer = 0
-
-        total_steps = 0
-
-        while first_pointer < len(nums) - 1:
-            effective_distance = nums[first_pointer] + first_pointer
-            if effective_distance >= len(nums) - 1:
-                total_steps += 1
-                break
-            index = first_pointer
-            for i in range(1, nums[first_pointer] + 1):
-                if i + first_pointer > len(nums) - 1:
-                    break
-                current_effective_distance = nums[first_pointer + i] + first_pointer + i
-                if current_effective_distance > effective_distance:
-                    effective_distance = current_effective_distance
-                    index = i + first_pointer
-            total_steps += 1
-            if index == first_pointer:
-                first_pointer += nums[first_pointer]
-            else:
-                first_pointer = index
-
-        return (total_steps)
-
+        if len(nums) == 1:
+            return 0
+        
+        visited = set()
+        
+        visited.add(0)
+        queue = collections.deque()
+        queue.append((0, 0))
+        
+        while queue:
+            top, steps = queue.popleft()
+            # else:
+            for i in range(nums[top]):
+                next_index = top + i + 1
+                if next_index == len(nums) - 1:
+                    return steps + 1
+                if next_index not in visited and next_index < len(nums):
+                    visited.add(next_index)
+                    queue.append((next_index, steps + 1))
+            # print(queue)
+        
+            
+        
+        
