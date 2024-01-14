3d33acd29a75d11a869f94de385db8fc26f9bacf
Sugeeth <raghava.sugeeth@gmail.com>
2024-01-13 23:15:56 -0700

    Time: 907 ms (99.82%), Space: 22.7 MB (40.27%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,22 +1,17 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         
-        effective = [0] * len(gas)
-        
-        for i in range(len(gas)):
-            effective[i] = gas[i] - cost[i]
-        
-        if sum(effective) < 0:
+        if sum(gas) - sum(cost) < 0:
             return -1
         
-        effective = effective + effective
         
-        max_ans = -1
-        max_index = -1
-        for i in range(len(effective) - 1, -1, -1):
-            effective[i] = max(0, effective[i] + (effective[i+1] if i + 1 < len(effective) else 0))
-            if effective[i] >= max_ans:
-                max_ans = effective[i]
-                max_index = i
-        return max_index % len(gas)
         
+        total = 0
+        res = 0
+        
+        for i in range(len(gas)):
+            total += (gas[i] - cost[i])
+            if total < 0:
+                total = 0
+                res = i + 1
+        return res
