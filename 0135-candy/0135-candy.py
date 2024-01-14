ad933ed28e61e6af6ab617dbc61dfa8003e1454e
Sugeeth <raghava.sugeeth@gmail.com>
2024-01-14 02:29:09 -0700

    Time: 135 ms (70.84%), Space: 20.3 MB (18.85%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,23 +1,26 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        combined = []
-        for i in range(len(ratings)):
-            combined.append((ratings[i],i))
-        combined.sort()
-        # print(combined)
-        seen = set()
-        ans = [0] * len(ratings)
-        for i in range(len(combined)):
-            number, index = combined[i]
-            current = 1
-            if index + 1 in seen:
-                if number > ratings[index + 1]:
-                    current = ans[index + 1] + 1
-            if index - 1 in seen:
-                if number > ratings[index - 1]:
-                    current = max(current, ans[index-1] + 1)
-            ans[index] = current
-            seen.add(index)
-        return sum(ans)
-                
         
+        '''
+        Algorithm
+        1. Create two sequences for increasing and decreasing count
+        2. Go over each of them and add the max at each index to the result.
+        '''
+        
+        increasing = [1] * len(ratings)
+        
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                increasing[i] = increasing[i-1] + 1
+        
+        decreasing = [1] * len(ratings)
+        
+        for i in range(len(ratings)-2,-1,-1):
+            if ratings[i] > ratings[i+1]:
+                decreasing[i] = decreasing[i+1] + 1
+        
+        # print(increasing, decreasing)
+        ans = 0
+        for i in range(len(increasing)):
+            ans = ans + max(increasing[i], decreasing[i])
+        return ans
