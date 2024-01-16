8277c1c8d2364968f081667aff81516d4cda4b54
Sugeeth <raghava.sugeeth@gmail.com>
2024-01-15 18:57:47 -0700

    Time: 119 ms (95.36%), Space: 20.2 MB (23.92%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,11 +1,6 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         
-        '''
-        Algorithm
-        1. Create two sequences for increasing and decreasing count
-        2. Go over each of them and add the max at each index to the result.
-        '''
         
         increasing = [1] * len(ratings)
         
@@ -13,14 +8,10 @@ class Solution:
             if ratings[i] > ratings[i-1]:
                 increasing[i] = increasing[i-1] + 1
         
-        decreasing = [1] * len(ratings)
-        
         for i in range(len(ratings)-2,-1,-1):
             if ratings[i] > ratings[i+1]:
-                decreasing[i] = decreasing[i+1] + 1
+                increasing[i] = max(increasing[i], increasing[i+1] + 1)
         
-        # print(increasing, decreasing)
-        ans = 0
-        for i in range(len(increasing)):
-            ans = ans + max(increasing[i], decreasing[i])
-        return ans
+        return sum(increasing)
+            
+        
