8ac0c31dede49abba274c02a91dead8aec860b42
Sugeeth <raghava.sugeeth@gmail.com>
2024-01-15 19:13:24 -0700

    Time: 132 ms (90.81%), Space: 18.8 MB (36.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,26 +1,16 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        """
-        Algorithm:
-        1. We will be doing boore moyre voting algorithm approach
-        2. We will start with first element and the number of votes it received as 1
-        3. If we encounter a different element, we reduce the number of votes of the current element
-        4. If the number of votes is already 0, we replace the current element with the new element and make its vote as 1
-        5. At the end, the element left behind in the current variable would be the majority element.
-        """
-        
+        votes = 1
         current = nums[0]
         
-        votes = 1
-        
         for i in range(1, len(nums)):
-            if nums[i] != current:
-                if votes == 0:
-                    current = nums[i]
-                    votes = 1
+            if votes == 0:
+                current = nums[i]
+                votes = 1
+            else:
+                if nums[i] == current:
+                    votes += 1
                 else:
                     votes -= 1
-            else:
-                votes += 1
         return current
         
