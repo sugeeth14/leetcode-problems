66fc021d790f6c0cf7b73f25dc307e6ae54637ca
Sugeeth <raghava.sugeeth@gmail.com>
2024-01-15 19:10:35 -0700

    Time: 46 ms (41.77%), Space: 17.5 MB (10.55%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,27 +3,30 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        last = m + n - 1
         
-        ptr2 = n - 1
+        r = m + n - 1
+        
         ptr1 = m - 1
+        ptr2 = n - 1
         
         while ptr1 >= 0 and ptr2 >= 0:
-            if nums1[ptr1] <= nums2[ptr2]:
-                # ptr2 must go at the end
-                nums1[last] = nums2[ptr2]
-                ptr2 -= 1
-            else:
-                nums1[last] = nums1[ptr1]
+            if nums1[ptr1] > nums2[ptr2]:
+                nums1[r] = nums1[ptr1]
                 ptr1 -= 1
-            last -= 1
+            else:
+                nums1[r] = nums2[ptr2]
+                ptr2 -= 1
+            
+            r -= 1
         
         while ptr1 >= 0:
-            nums1[last] = nums1[ptr1]
-            last -= 1
+            nums1[r] = nums1[ptr1]
+            r -= 1
             ptr1 -= 1
+        
         while ptr2 >= 0:
-            nums1[last] = nums2[ptr2]
-            last -= 1
+            nums1[r] = nums2[ptr2]
+            r -= 1
             ptr2 -= 1
+        
         
