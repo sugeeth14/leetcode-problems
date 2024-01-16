b5a141c918ed4e3b1486cfaf41b8e3ded644b458
Sugeeth <raghava.sugeeth@gmail.com>
2024-01-15 18:50:20 -0700

    Time: 110 ms (79.97%), Space: 18.5 MB (29.75%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,18 +1,19 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        
+        
+        
         l = 0
         r = 0
-        
         res = 0
-        
         while r < len(nums) - 1:
             max_distance = 0
-            
             for i in range(l, r + 1):
                 max_distance = max(max_distance, nums[i] + i)
             
+            res += 1
             l = r + 1
             r = max_distance
-            res += 1
+        
         return res
         
