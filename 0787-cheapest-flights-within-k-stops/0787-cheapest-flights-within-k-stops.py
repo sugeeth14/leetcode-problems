3a6d23fb38297de4807f3a5e2e7221477992fb81
Sugeeth <raghava.sugeeth@gmail.com>
2024-02-22 20:07:07 -0700

    Time: 86 ms (94.60%), Space: 18.7 MB (30.82%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,38 +1,57 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        ans = inf
+        '''
+        Algorithm:
+        1. We will be doing this in BFS manner
+        2. We will initialize a res variable to inf which holdest the cheapest price.
+        3. We also create a queue to do the bfs.
+        4. We initially add all the children of the source to the queue. With steps and cost
+        5. We keep popping from the queue and if the child is not visited, we will visit its children.
+        6. We will also visit if cost to reach a node is less than the existing cost
+        6. Once we reach the destination we update the res
+        7. We add the children only if the current cost is less than res.
+        '''
         
-        neighbors = collections.defaultdict(dict)
-        for flight in flights:
-            neighbors[flight[0]][flight[1]] = flight[2]
-        # print(neighbors)
-        queue = collections.deque()
-        queue.append((0, 0, src))
-        ans = inf
         res = {}
+        # initially every node is at a distance of inf from the source node. 
+        for i in range(n):
+            res[i] = float('inf')
+        
+        # update res from source to source as zero
+        res[src] = 0
+        
+        queue = collections.deque()
+        visited = set()
+        
+        # construct an adjacency matrix of children nodes.
+        adj = collections.defaultdict(list)
+        for flight in flights:
+            adj[flight[0]].append([1, flight[1], flight[2]]) # add the flight to the adj list as children.
+        
+        # add the source flights children to the queue
+        for child in adj[src]:
+            queue.append(child)
+        
         while queue:
-            distance, stops, node = queue.popleft()
-            if node == dst:
-                ans = min(ans, distance)
+            steps, stop, cost = queue.popleft()
+            if stop in visited and cost >= res[stop]:
+                # We need not do anything as this is the best possible way to reach the stop
+                pass
             else:
-                if stops == k + 1:
-                    continue
+                visited.add(stop) # The stop has been visited. 
+                # update the res of the stop
+                res[stop] = cost
+                if steps == k + 1:
+                    pass # Do nothing as we have aleady reached the maximum possible steps
                 else:
-                    # if node in res:
-                    #     if distance > res[node][0] and stops > res[node][1]:
-                    #         continue
-                    # else:
-                    #     res[node] = [distance, stops]
-                    # else:
-                        # append the neighbors
-                    if distance > ans:
-                        continue
-                    if node not in res:
-                        res[node] = [distance, stops]
-                    else:
-                        if distance > res[node][0] and stops > res[node][1]:
-                            continue
-                    for neighbor in neighbors[node]:
-                        queue.append((distance + neighbors[node][neighbor], stops + 1, neighbor))
-        return ans if ans != inf else -1
+                    # Go to the next level of bfs
+                    for child in adj[stop]:
+                        
+                        queue.append([steps + 1, child[1], child[2] + res[stop]])
+        if dst in visited:
+            return res[dst]
+        return -1
+                
+            
+            
         
