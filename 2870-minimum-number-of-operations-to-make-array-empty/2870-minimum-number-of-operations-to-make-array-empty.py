012ff3073d0e7c1b0d8221f2d3bf399ed3c37473
Sugeeth <raghava.sugeeth@gmail.com>
2024-05-14 21:09:58 -0600

    Time: 598 ms (33.86%), Space: 31 MB (39.25%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
@@ -1,21 +1,33 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
+        '''
+        Algorithm:
+        1. Keep a count of frequencies of each element in the array.
+        2. Iterate through the count, if the count is 1 return -1.
+        3. Else, check the mod by 3 for the count.
+        4. If mod is zero divide the count by 3 and add it to the res.
+        5. If the mod is 2, lower divide the count and add 1 to it.
+        6. If the mod is 1, subtract 4 from it and divide by 3 and add to the res and add 2 to the res to include for the 4.
+        '''
+        res = 0
         
         dic = collections.defaultdict(int)
         
-        for i in range(len(nums)):
-            dic[nums[i]] += 1
+        for num in nums:
+            dic[num] += 1
         
-        res = 0
-        
-        for value in dic.values():
-            if value == 1:
+        for num in dic:
+            if dic[num] == 1:
                 return -1
             else:
-                while value % 3 != 0:
-                    value -= 2
-                    res += 1
-                res = res + (value)//3
+                count = dic[num]
+                remainder = count % 3
+                if remainder == 0:
+                    res = res + (count // 3)
+                elif remainder == 2:
+                    res = res + (count // 3) + 1
+                else:
+                    count -= 4
+                    res = res + (count // 3) + 2
         return res
-            
         
