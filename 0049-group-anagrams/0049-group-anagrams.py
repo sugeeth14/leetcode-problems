64d4707f321f7bc5265b4a8004b34b22df2a93ca
Sugeeth <raghava.sugeeth@gmail.com>
2024-05-15 18:46:27 -0600

    Time: 86 ms (73.89%), Space: 20.2 MB (52.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,22 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        '''
+        Algorithm:
+        1. Sort each word in str
+        2. Iterate over each word if the word is already in dictionary add to the list in that dictionary key
+        3. If not create an empty list with and the word into the list and as the key.
+        '''
         
-        
-        sorted_strings = [""] * len(strs)
+        sorted_strs = []
         for i in range(len(strs)):
-            sorted_strings[i] = "".join(sorted(strs[i]))
-        # print(strs)
-        res = []
-        dic = collections.defaultdict(list)
+            sorted_strs.append("".join(sorted(strs[i])))
         
-        for i in range(len(sorted_strings)):
-            dic[sorted_strings[i]].append(strs[i])
+        dic = {}
         
-        for key in dic:
-            res.append(dic[key])
-        return res
+        for i in range(len(strs)):
+            if sorted_strs[i] in dic:
+                dic[sorted_strs[i]].append(strs[i])
+            else:
+                dic[sorted_strs[i]] = [strs[i]]
+        return dic.values()
+        
