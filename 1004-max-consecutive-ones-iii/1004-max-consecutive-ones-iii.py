aa55a31af68c10292773ed639e6847052d5840e0
Sugeeth <raghava.sugeeth@gmail.com>
2024-09-26 20:29:29 -0600

    Time: 452 ms (32.41%), Space: 17.2 MB (18.03%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -1,30 +1,20 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         
-        '''
-        Algorithm
-        1. We will be doing this as a sliding window approach
-        2. Initialize two pointers l and r
-        3. Keep incrementing r and when you reach the number of zeros in between greater than k stop.
-        4. Keep incrementing l until it reaches r and stop if it overcomes it. 
-        5. At every stage keep count of the maximum window by calculating r - l + 1 if the number of zeros in the range are <= k
-        '''
-        
+        zeros = 0
         l = 0
+        r = 0
         ans = 0
-        
-        current = 0
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                current += 1
-            
-            while l <= i and current > k:
+        while r < len(nums):
+            if nums[r] == 0:
+                zeros += 1
+            r += 1
+            while l < r and zeros > k:
                 if nums[l] == 0:
-                    current -= 1
+                    zeros -= 1
                 l += 1
-            
-            ans = max(ans, i - l + 1)
+            ans = max(ans, r - l)
+            # print(l, r)
         return ans
-                        
             
         
